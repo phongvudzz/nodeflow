c87f52f7ce38e37ea08be21c2207e672a10fd744
phongvudz <phongvulearning@gmail.com>
2025-11-23 16:16:42 +0700

    06: background jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,30 @@
-import { requireAuth } from "@/lib/auth-utils";
+"use client";
+import { useTRPC } from "@/trpc/client";
 import { LogoutButton } from "./logout";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { Button } from "@/components/ui/button";
 
-async function Page() {
-  await requireAuth();
+function Page() {
+  const trpc = useTRPC();
+  const queryClient = useQueryClient();
+  const { data } = useQuery(trpc.getWorkflows.queryOptions());
+
+  const create = useMutation(
+    trpc.createWorkflow.mutationOptions({
+      onSuccess: () => {
+        queryClient.invalidateQueries(trpc.getWorkflows.queryOptions());
+      },
+    })
+  );
 
   return (
-    <div className="size-full bg-black text-red-500 flex items-center justify-center h-screen">
+    <div className="flex flex-col gap-4 size-full bg-black text-red-500 items-center justify-center h-screen">
       protected server component
       <LogoutButton />
+      <Button onClick={() => create.mutate()} disabled={create.isPending}>
+        Create Workflow
+      </Button>
+      <div>{JSON.stringify(data, null, 2)}</div>
     </div>
   );
 }
