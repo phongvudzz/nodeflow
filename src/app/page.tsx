98ab09527ea22460509ba7bd2d0203272afb1ef3
Phong Vu <vu.nguyenp@s3corp.com.vn>
2025-11-24 17:53:33 +0700

    07: ai providers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { useTRPC } from "@/trpc/client";
 import { LogoutButton } from "./logout";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { Button } from "@/components/ui/button";
+import { toast } from "sonner";
 
 function Page() {
   const trpc = useTRPC();
@@ -17,12 +18,23 @@ function Page() {
     })
   );
 
+  const testAI = useMutation(
+    trpc.testAi.mutationOptions({
+      onSuccess: () => {
+        toast.success("Test AI success");
+      },
+    })
+  );
+
   return (
     <div className="flex flex-col gap-4 size-full bg-black text-red-500 items-center justify-center h-screen">
       protected server component
       <LogoutButton />
       <Button onClick={() => create.mutate()} disabled={create.isPending}>
         Create Workflow
+      </Button>
+      <Button onClick={() => testAI.mutate()} disabled={testAI.isPending}>
+        Test AI
       </Button>
       <div>{JSON.stringify(data, null, 2)}</div>
     </div>
