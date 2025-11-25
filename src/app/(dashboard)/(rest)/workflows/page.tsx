51393be95015aeec46381cb58927b84e0cfdfa77
phongvudz <phongvulearning@gmail.com>
2025-11-26 01:43:13 +0700

    12: workflows pagination

--- src/app/(dashboard)/(rest)/workflows/page.tsx
+++ src/app/(dashboard)/(rest)/workflows/page.tsx
@@ -7,11 +7,18 @@ import {
   WorkflowList,
   WorkflowsContainer,
 } from "@/features/workflows/components/workflows";
+import { SearchParams } from "nuqs/server";
+import { workflowsParamsLoader } from "@/features/workflows/server/params-loader";
 
-const Page = async () => {
+type Props = {
+  searchParams: Promise<SearchParams>;
+};
+const Page = async ({ searchParams }: Props) => {
   await requireAuth();
 
-  prefetchWorkflows();
+  const params = await workflowsParamsLoader(searchParams);
+
+  prefetchWorkflows(params);
 
   return (
     <WorkflowsContainer>
