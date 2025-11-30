061b2fa8436bfa20c3bc60f5fd6738b4f634d6c5
phongvudzz <phongvulearning@gmail.com>
2025-11-30 15:21:30 +0700

    15: Add react flow and transform nodes, edgdes

--- src/features/editor/components/editor.tsx
+++ src/features/editor/components/editor.tsx
@@ -2,6 +2,20 @@
 
 import { ErrorView, LoadingView } from "@/components/entity-components";
 import { useSuspenseWorkflow } from "@/features/workflows/hooks/use-workflows";
+import { useState, useCallback } from "react"
+import {
+    ReactFlow, applyNodeChanges, applyEdgeChanges, addEdge, Background, Controls, type Node,
+    type Edge,
+    type FitViewOptions,
+    type OnConnect,
+    type OnNodesChange,
+    type OnEdgesChange,
+    type OnNodeDrag,
+    type DefaultEdgeOptions,
+    MiniMap,
+} from '@xyflow/react';
+import '@xyflow/react/dist/style.css';
+
 
 
 interface EditorStateProps {
@@ -16,9 +30,48 @@ export const EditorError = ({ message }: EditorStateProps) => {
     return <ErrorView message={message} />;
 }
 
-export const Editor = ({ workflowId }: { workflowId: string }) => {
-    const workflow = useSuspenseWorkflow(workflowId);
+const fitViewOptions: FitViewOptions = {
+    padding: 0.2,
+};
 
-    return <div>{JSON.stringify(workflow, null, 2)}
+const defaultEdgeOptions: DefaultEdgeOptions = {
+    animated: true,
+};
+
+
+export const Editor = ({ workflowId }: { workflowId: string }) => {
+    const { data: workflow } = useSuspenseWorkflow(workflowId);
+    const [nodes, setNodes] = useState(workflow.nodes);
+    const [edges, setEdges] = useState(workflow.edges);
+
+    const onNodesChange: OnNodesChange = useCallback(
+        (changes) => setNodes((nds) => applyNodeChanges(changes, nds)),
+        [setNodes],
+    );
+    const onEdgesChange: OnEdgesChange = useCallback(
+        (changes) => setEdges((eds) => applyEdgeChanges(changes, eds)),
+        [setEdges],
+    );
+    const onConnect: OnConnect = useCallback(
+        (connection) => setEdges((eds) => addEdge(connection, eds)),
+        [setEdges],
+    );
+
+    return <div className="size-full">
+        <ReactFlow
+            nodes={nodes}
+            edges={edges}
+            onNodesChange={onNodesChange}
+            onEdgesChange={onEdgesChange}
+            onConnect={onConnect}
+            fitView
+            fitViewOptions={fitViewOptions}
+            defaultEdgeOptions={defaultEdgeOptions}
+        >
+            <Background />
+            <Controls />
+            <MiniMap />
+        </ReactFlow>
+
     </div>
 }
